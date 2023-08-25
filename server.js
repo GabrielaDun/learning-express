2fbce3bc68bfd0a90f9c02b53e5910e406493c5f
GabrielaDun <123959816+GabrielaDun@users.noreply.github.com>
2023-08-25 09:29:53 +0200

    Added handlebars layouts

--- server.js
+++ server.js
@@ -1,46 +1,44 @@
 const express = require('express');
 const path = require('path');
-
-const app = express();
-
 const hbs = require('express-handlebars');
 
+const app = express();
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
-
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
-});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 app.get('/home', (req, res) => {
-  res.show('index.html');
+  res.render('index');
 });
 
-app.use('/user', (req, res, next) => {
-  if(isAdmin()) next();
-  else res.show('login.html');
-});
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about');
 });
 
 app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
+  res.render('hello', { name: req.params.name });
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.use((req, res) => {
-  res.status(404).show('404error.html');
+  res.status(404).send('404 not found...');
 })
-
 
 app.listen(8000, () => {
   console.log('Server is running on port: 8000');
